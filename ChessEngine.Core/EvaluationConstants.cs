584934363a4d0310197452425aab15dbe597cca1
unknown <olegprokofyev2003@gmail.com>
2024-04-24 14:41:44 -0400

    Basic Engine Core Setup

---
+++ ChessEngine.Core/EvaluationConstants.cs
@@ -0,0 +1,153 @@
+﻿using Chess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessEngine.Core
+{
+    internal static class EvaluationConstants
+    {
+        public static Dictionary<PieceType, int> PieceValue { get; } = new()
+        {
+            { PieceType.Pawn, 10 },
+            { PieceType.Knight, 30 },
+            { PieceType.Bishop, 30 },
+            { PieceType.Rook, 50 },
+            { PieceType.Queen, 90 },
+            { PieceType.King, 900 }
+        };
+
+        public static double[,] PawnPositionWeightsWhite { get; } = new double[,]
+        {
+            { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
+            { 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0 },
+            { 1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0 },
+            { 0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5 },
+            { 0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0 },
+            { 0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5 },
+            { 0.5, 1.0, 1.0, -2.0, -2.0, 1.0, 1.0, 0.5 },
+            { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }
+        };
+
+        public static double[,] KnightPositionWeights { get; } = new double[,]
+        {
+            { -5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0 },
+            { -4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0 },
+            { -3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0 },
+            { -3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0 },
+            { -3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0 },
+            { -3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0 },
+            { -4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0 },
+            { -5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0 }
+        };
+
+        public static double[,] BishopPositionWeightsWhite { get; } = new double[,]
+        {
+            { -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0 },
+            { -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0 },
+            { -1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0 },
+            { -1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0 },
+            { -1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0 },
+            { -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0 },
+            { -1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0 },
+            { -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0 }
+        };
+
+        public static double[,] RookPositionWeightsWhite { get; } = new double[,]
+        {
+            { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
+            { 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5 },
+            { -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5 },
+            { -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5 },
+            { -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5 },
+            { -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5 },
+            { -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5 },
+            { 0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0 }
+        };
+
+        public static double[,] QueenPositionWeights { get; } = new double[,]
+        {
+            { -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0 },
+            { -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0 },
+            { -1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0 },
+            { -0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5 },
+            { 0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5 },
+            { -1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0 },
+            { -1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0 },
+            { -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0 }
+        };
+
+        public static double[,] KingPositionWeightsWhite { get; } = new double[,]
+        {
+            { -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0 },
+            { -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0 },
+            { -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0 },
+            { -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0 },
+            { -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0 },
+            { -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0 },
+            { 2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0 },
+            { 2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0 }
+        };
+
+        public static double[,] PawnPositionWeightsBlack { get; } = ReverseArray(PawnPositionWeightsWhite);
+        public static double[,] BishopPositionWeightsBlack { get; } = ReverseArray(BishopPositionWeightsWhite);
+        public static double[,] RookPositionWeightsBlack { get; } = ReverseArray(RookPositionWeightsWhite);
+        public static double[,] KingPositionWeightsBlack { get; } = ReverseArray(KingPositionWeightsWhite);
+
+        public static Dictionary<PieceType, Dictionary<PieceColor, double[,]>> PositionWeights { get; } = new() 
+        { 
+            { 
+                PieceType.Pawn, new() 
+                { 
+                    { PieceColor.White, PawnPositionWeightsWhite }, 
+                    { PieceColor.Black, PawnPositionWeightsBlack } 
+                } 
+            }, 
+            { 
+                PieceType.Rook, new() 
+                { 
+                    { PieceColor.White, RookPositionWeightsWhite }, 
+                    { PieceColor.Black, RookPositionWeightsBlack } 
+                }
+            },
+            {
+                PieceType.Knight, new()
+                {
+                    { PieceColor.White, KnightPositionWeights },
+                    { PieceColor.Black, KnightPositionWeights }
+                }
+            },
+            {
+                PieceType.Bishop, new()
+                {
+                    { PieceColor.White, BishopPositionWeightsWhite },
+                    { PieceColor.Black, BishopPositionWeightsBlack }
+                }
+            },
+            {
+                PieceType.Queen, new()
+                {
+                    { PieceColor.White, QueenPositionWeights },
+                    { PieceColor.Black, QueenPositionWeights }
+                }
+            },
+            {
+                PieceType.King, new()
+                {
+                    { PieceColor.White, KingPositionWeightsWhite },
+                    { PieceColor.Black, KingPositionWeightsBlack }
+                }
+            },
+        };
+
+        private static double[,] ReverseArray(double[,] array)
+        {
+            var outputArray = new double[8, 8]; 
+            array.CopyTo(outputArray, 0);
+            Array.Reverse(outputArray);
+            return outputArray;
+        }
+    }
+}
---
+++ ChessEngine.Core/MinimaxEngine.cs
@@ -0,0 +1,98 @@
+﻿using Chess;
+using System.Drawing;
+
+namespace ChessEngine.Core
+{
+    public class MinimaxEngine
+    {
+        private readonly ChessBoard _board;
+
+        public MinimaxEngine()
+        {
+            _board = new();
+        }
+
+        public Move CalculateMove(Move opponentMove)
+        {
+            _board.Move(opponentMove);
+            var possibleMoves = _board.Moves();
+            bool maximumPlayer = _board.Turn == PieceColor.White;
+            Dictionary<Move, double> moveEvals = new Dictionary<Move, double>();
+
+            foreach (Move move in possibleMoves)
+            {
+                double val = PerformMinimax(move, 5, double.NegativeInfinity, double.PositiveInfinity, maximumPlayer);
+                moveEvals[move] = val;
+            }
+
+            Move bestMove = moveEvals.OrderBy(x => x.Value).First().Key;
+            return bestMove;
+        }
+
+        private double PerformMinimax(Move evaluationMove, int depth, double alpha, double beta, bool isMaximizingPlayer)
+        {
+            _board.Move(evaluationMove);
+            var possibleMoves = _board.Moves();
+            if(depth == 0 || _board.IsEndGame)
+            {
+                var value = EvaluateBoardPositions();
+                _board.Cancel();
+                return value;
+            }
+
+            if (isMaximizingPlayer)
+            {
+                double maxEval = double.NegativeInfinity;
+                foreach (var move in possibleMoves)
+                {
+                    double eval = PerformMinimax(move, depth - 1, alpha, beta, false);
+                    maxEval = Math.Max(maxEval, eval);
+                    alpha = Math.Max(alpha, eval);
+                    if (beta <= alpha)
+                    {
+                        break;
+                    }
+                }
+                _board.Cancel();
+                return maxEval;
+            }
+            else
+            {
+                double minEval = double.PositiveInfinity;
+                foreach (var move in possibleMoves)
+                {
+                    double eval = PerformMinimax(move, depth - 1, alpha, beta, true);
+                    minEval = Math.Min(minEval, eval);
+                    beta = Math.Min(beta, eval);
+                    if (beta <= alpha)
+                    {
+                        break;
+                    }
+                }
+                _board.Cancel();
+                return minEval;
+            }
+
+
+        }
+
+        private double EvaluateBoardPositions()
+        {
+            var score = 0d;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    var piece = _board[i, j];
+                    if(piece != null)
+                    {
+                        var positionWeights = EvaluationConstants.PositionWeights[piece.Type][piece.Color];
+                        score += positionWeights[i, j];
+                        score += EvaluationConstants.PieceValue[piece.Type] * (piece.Color == PieceColor.Black ? -1 : 1);
+                    }
+                }
+            }
+            return score;
+        }
+    }
+}
